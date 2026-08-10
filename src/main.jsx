5144322af37e4952cea6a357d495bc7e4e057939
vyas10102 <144558776+vyas10102@users.noreply.github.com>
2026-08-10 15:12:39 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import { StrictMode } from "react";
-
-import {
-  createRoot,
-} from "react-dom/client";
-
-import {
-  BrowserRouter,
-} from "react-router";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
 import "./index.css";
-
 import App from "./App.jsx";
 
-createRoot(
-  document.getElementById("root")
-).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio2">
       <App />
     </BrowserRouter>
   </StrictMode>
